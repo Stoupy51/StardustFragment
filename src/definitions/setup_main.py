b642eaae0004a32f450889863bdb6c127a486ec1
Stoupy51 <stoupy51@gmail.com>
2025-10-14 22:59:49 +0200

    wip: 🚧 82.7% migration to StewBeet

--- src/definitions/setup_main.py
+++ src/definitions/setup_main.py
@@ -20,8 +20,7 @@ from .manual_assets import manual_assets_main
 
 # Constants
 DIAMOND_PICKAXE: float = VanillaEquipments.PICKAXE.value[DefaultOre.DIAMOND]["durability"]
-STARDUST_BASE_DURABILITY: float = 1.25 * DIAMOND_PICKAXE
-REINFORCED_DURABILITY: float = 1.5 * DIAMOND_PICKAXE
+STARDUST_BASE_DURABILITY: float = 1.5 * DIAMOND_PICKAXE
 ORIGINAL_DURABILITY: float = 3.0 * DIAMOND_PICKAXE
 LEGENDARIUM_DURABILITY: float = 4.5 * DIAMOND_PICKAXE
 SOLARIUM_DURABILITY: float = 5.0 * DIAMOND_PICKAXE
@@ -29,9 +28,8 @@ DARKIUM_DURABILITY: float = 5.5 * DIAMOND_PICKAXE
 
 ORES_CONFIGS: dict[str, EquipmentsConfig|None] = {
 	"awakened_stardust!":	None,
-	"damaged_stardust!":	EquipmentsConfig(DefaultOre.DIAMOND, STARDUST_BASE_DURABILITY, {"attack_damage": 1, "armor": 0.5, "armor_toughness": 0.5, "mining_efficiency": 1.25}),
-	"reinforced_stardust!":	EquipmentsConfig(DefaultOre.DIAMOND, REINFORCED_DURABILITY, {"attack_damage": 5, "armor": 1.5, "armor_toughness": 1.0, "mining_efficiency": 1.5}),
-	"original_stardust!":	EquipmentsConfig(DefaultOre.NETHERITE, ORIGINAL_DURABILITY, {"attack_damage": 9, "armor": 3.5, "armor_toughness": 1.5, "mining_efficiency": 2.0}),
+	"damaged_stardust!":	EquipmentsConfig(DefaultOre.DIAMOND, STARDUST_BASE_DURABILITY, {"attack_damage": 2, "armor": 1.0, "armor_toughness": 1.0, "mining_efficiency": 1.5}),
+	"original_stardust!":	EquipmentsConfig(DefaultOre.DIAMOND, ORIGINAL_DURABILITY, {"attack_damage": 8, "armor": 3.0, "armor_toughness": 1.3, "mining_efficiency": 2.0}),
 	"legendarium_ingot":	EquipmentsConfig(DefaultOre.NETHERITE, LEGENDARIUM_DURABILITY, {"attack_damage": 13, "armor": 5.5, "armor_toughness": 1.6, "mining_efficiency": 3.0}),
 	"solarium_ingot":		EquipmentsConfig(DefaultOre.NETHERITE, SOLARIUM_DURABILITY, {"attack_damage": 15, "armor": 6.0, "armor_toughness": 1.8, "mining_efficiency": 3.0}),
 	"darkium_ingot":		EquipmentsConfig(DefaultOre.NETHERITE, DARKIUM_DURABILITY, {"attack_damage": 17, "armor": 6.5, "armor_toughness": 2.0, "mining_efficiency": 3.0}),

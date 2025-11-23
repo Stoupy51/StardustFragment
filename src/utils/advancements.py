0fe70c48e63fff01701aa12b6fdc718324185ece
Stoupy51 <stoupy51@gmail.com>
2025-11-23 15:28:43 +0100

    wip: 🚧 99.2% migration to StewBeet

--- src/utils/advancements.py
+++ src/utils/advancements.py
@@ -3,10 +3,10 @@
 # Imports
 from typing import Any
 
-from stewbeet import Advancement, JsonDict, Mem, Texture, set_json_encoder, super_merge_dict
+from stewbeet import Advancement, JsonDict, Mem, TextComponent, Texture, item_id_to_name, item_id_to_text_component, set_json_encoder, super_merge_dict, text_component_to_str
 from stouputils.print import error
 
-from ..definitions.additions.energy import quarry_display
+from ..definitions.additions.energy import QUARRY_STATS, quarry_display
 from ..definitions.additions.equipments import ARTIFACTS, SNIPER_BULLETS, artifact_display
 from ..definitions.additions.materials import COBBLESTONE_TIERS
 from ..definitions.additions.miscellaneous import miner_display
@@ -14,7 +14,6 @@ from .common import ROMAN_NUMERALS, STARFRAG_LIST
 
 # Constants
 IMPOSSIBLE_CRITERIA: JsonDict = {"requirement": {"trigger": "minecraft:impossible", "conditions": {}}}
-
 
 # Utility function to convert definition to icon
 def def_to_icon(item: JsonDict | str, enchanted: bool = False) -> JsonDict:
@@ -47,6 +46,24 @@ def def_to_icon(item: JsonDict | str, enchanted: bool = False) -> JsonDict:
 		icon["components"]["minecraft:enchantment_glint_override"] = True
 	return icon
 
+def criteria_item(item: str) -> JsonDict:
+	criteria: dict[str, Any] = {"requirement": {"trigger": "minecraft:inventory_changed","conditions": {"items": [{"predicates": {}}]}}}
+	criteria["requirement"]["conditions"]["items"][0]["predicates"]["minecraft:custom_data"] = {Mem.ctx.project_id:{item:True}}
+	return criteria
+
+def default_description(item: str) -> TextComponent:
+	text: TextComponent = item_id_to_text_component(item)
+	name: str = text_component_to_str(text)
+	obtain: str = "a " if (name[0].lower() not in "aeiou") else "an "
+	if isinstance(text, str):
+		return {"text": f"Obtain {obtain}{name}", "color": "green"}
+	else:
+		return [{"text": f"Obtain {obtain}", "color": "green"}, text]
+
+def sort_dict(d: JsonDict, order: list[str]) -> JsonDict:
+	return dict(sorted(d.items(), key=lambda x: order.index(x[0]) if x[0] in order else len(order)))
+
+
 # Add visible advancements to the datapack
 def add_visible_advancements() -> None:
 	ns: str = Mem.ctx.project_id
@@ -76,7 +93,7 @@ def add_visible_advancements() -> None:
 		## Adventure advancements
 		# Artifacts
 		**{
-			f"adventure/artifacts/{item}": {"display": {"title": {"text": f"Increasing {name} {roman}", "color": "aqua"}, "description": {"text": f"Obtain a level {i+1} {artifact.title()} Artifact", "color": "green"}}, "parent": parent}
+			f"adventure/artifacts/{item}": {"display": {"title": {"text": f"Increasing {name} {roman}", "color": "aqua"}}, "parent": parent}
 			for artifact, _, attribute, levels in ARTIFACTS
 			for name in (attribute.replace("_", " ").title(),)
 			for i in range(len(levels))
@@ -86,9 +103,8 @@ def add_visible_advancements() -> None:
 
 		# Seeds
 		**{
-			f"adventure/seeds/{seed_id}": {"display": {"title": {"text": title, "color": "aqua"}, "description": {"text": f"Admire this {name} Seed", "color": "green"}}, "parent": parent}
+			f"adventure/seeds/{seed_id}": {"display": {"title": {"text": title, "color": "aqua"}, "description": {"text": f"Admire this {item_id_to_name(seed_id)}", "color": "green"}}, "parent": parent}
 			for i, (seed_id, title) in enumerate(SEEDS)
-			for name in (seed_id.replace("_", " ").title(),)
 			for parent in (f"{ns}:visible/root" if i == 0 else f"{ns}:visible/adventure/seeds/{SEEDS[i-1][0]}",)
 		},
 
@@ -96,7 +112,7 @@ def add_visible_advancements() -> None:
 		**{
 			f"adventure/bullets/{bullet.replace('minecraft:', '')}": {"display": {"icon": icon, "title": {"text": title, "color": "aqua"}, "description": {"text": f"Shoot a {name} Bullet using a Sniper", "color": "green"}}, "parent": parent, "criteria": IMPOSSIBLE_CRITERIA}
 			for i, bullet in enumerate(SNIPER_BULLETS.keys())
-			for name in (bullet.replace("minecraft:", "").replace("_", " ").title(),)
+			for name in (item_id_to_name(bullet),)
 			for title in (f"{name.split(' ')[0]} Bullets" if bullet != "stardust_fragment" else "Shooting Stars",)
 			for icon in (def_to_icon(bullet),)
 			for parent in (f"{ns}:visible/stuff/stardust_sniper" if i == 0 else f"{ns}:visible/adventure/bullets/{list(SNIPER_BULLETS.keys())[i-1].replace('minecraft:', '')}",)
@@ -126,7 +142,7 @@ def add_visible_advancements() -> None:
 		## Stuff advancements
 		# Compressed cobblestones
 		**{
-			f"stuff/cobblestone/{tier}": {"display": {"icon": def_to_icon(f"{tier}_cobblestone"), "title": {"text": title, "color": "aqua"}, "description": {"text": f"Obtain a {tier.replace('_', ' ').title()} Cobblestone", "color": "green"}, "frame": frame}, "parent": parent}
+			f"stuff/cobblestone/{tier}": {"display": {"icon": def_to_icon(f"{tier}_cobblestone"), "title": {"text": title, "color": "aqua"}, "description": default_description(f"{tier}_cobblestone"), "frame": frame}, "parent": parent}
 			for i, tier in enumerate(COBBLESTONE_TIERS)
 			for title in (f"Compressing {ROMAN_NUMERALS[i]}" if i < (len(COBBLESTONE_TIERS) - 1) else "43,046,721",)
 			for frame in ("task" if i < (len(COBBLESTONE_TIERS) - 1) else "challenge",)
@@ -134,10 +150,10 @@ def add_visible_advancements() -> None:
 		},
 		# Cobblestone Miners
 		**{
-			f"stuff/miners/{item}": {"display": {"title": {"text": f"Rockraiser {roman}", "color": "aqua"}, "description": {"text": f"Obtain a {name}", "color": "green"}, "frame": frame}, "parent": parent}
+			f"stuff/miners/{item}": {"display": {"title": {"text": f"Rockraiser {roman}", "color": "aqua"}, "frame": frame}, "parent": parent}
 			for i in range(8)
 			for frame in ("task" if i < 7 else "challenge",)
-			for item, name, roman in (miner_display(i),)
+			for item, _, roman in (miner_display(i),)
 			for parent in (f"{ns}:visible/stuff/cobblestone/compressed" if i == 0 else f"{ns}:visible/stuff/miners/{miner_display(i-1)[0]}",)
 		},
 
@@ -173,38 +189,65 @@ def add_visible_advancements() -> None:
 		"stuff/ultimate_sniper": {"display": {"title": {"text": "Trick Shot III", "color": "aqua"}, "description": {"text": "A whisper before the storm of bullets coming from the Ultimate Sniper", "color": "green"}, "frame": "challenge"}, "parent": f"{ns}:visible/stuff/awakened_stardust_sniper"},
 
 		# Remaining equipments
-		"stuff/item_magnet": {"display": {"title": {"text": "I'm Attractive", "color": "aqua"}, "description": {"text": "Obtain an Item Magnet", "color": "green"}}, "parent": f"{ns}:visible/stuff/ultimate_shard"},	# TODO: Add functionnality to the magnet
+		"stuff/item_magnet": {"display": {"title": {"text": "I'm Attractive", "color": "aqua"}}, "parent": f"{ns}:visible/stuff/ultimate_shard"},	# TODO: Add functionnality to the magnet
 		"stuff/home_travel_staff": {"display": {"title": {"text": "I'm Out of There", "color": "aqua"}, "description": {"text": "Use a Travel Staff for the first time", "color": "green"}}, "parent": f"{ns}:visible/stuff/dragon_pearl"},
 		"stuff/wormhole_potion": {"display": {"title": {"text": "Where are you my friend", "color": "aqua"}, "description": {"text": "Use a Wormhole Potion for the first time", "color": "green"}}, "parent": f"{ns}:visible/stuff/compacted_stardust_shard"},
-		"stuff/stardust_apple": {"display": {"title": {"text": "Cosmic Apple", "color": "aqua"}, "description": {"text": "Obtain a Stardust Apple", "color": "green"}}, "parent": f"{ns}:visible/root"},
+		"stuff/stardust_apple": {"display": {"title": {"text": "Cosmic Apple", "color": "aqua"}}, "parent": f"{ns}:visible/root"},
 		"stuff/life_crystal": {"display": {"title": {"text": "Increasing Health", "color": "aqua"}, "description": {"text": "Eat your first Life Crystal", "color": "green"}}, "parent": f"{ns}:visible/stuff/stardust_apple", "criteria": IMPOSSIBLE_CRITERIA},
 		"stuff/life_crystal_max": {"display": {"icon": def_to_icon("life_crystal", enchanted=True), "title": {"text": "Two Health Bars!", "color": "aqua"}, "description": {"text": "Consume your 20th and final Life Crystal", "color": "green"}}, "parent": f"{ns}:visible/stuff/life_crystal", "criteria": IMPOSSIBLE_CRITERIA},
 
 		# Other stuff
-		"stuff/dragon_pearl": {"display": {"title": {"text": "Faster and Farther", "color": "aqua"}, "description": {"text": "Obtain a Dragon Pearl", "color": "green"}}, "parent": f"{ns}:visible/stuff/stardust_essence"},
-		"stuff/ender_dragon_pearl": {"display": {"title": {"text": "Even Faster and Farther", "color": "aqua"}, "description": {"text": "Obtain an Ender Dragon Pearl", "color": "green"}}, "parent": f"{ns}:visible/stuff/dragon_pearl"},
-		"stuff/stardust_block": {"display": {"title": {"text": "Is that Stardust Diamond?", "color": "aqua"}, "description": {"text": "Obtain a Stardust Block", "color": "green"}}, "parent": f"{ns}:visible/stuff/stardust_ingot"},
-		"stuff/compacted_stardust_shard": {"display": {"title": {"text": "A costly Stardust Energy", "color": "aqua"}, "description": {"text": "Obtain a Compacted Stardust Shard", "color": "green"}}, "parent": f"{ns}:visible/stuff/stardust_block"},
-		"stuff/stardust_ingot": {"display": {"title": {"text": "I feel like it's useful", "color": "aqua"}, "description": {"text": "Obtain a Stardust Ingot", "color": "green"}}, "parent": f"{ns}:visible/root"},
-		"stuff/stardust_essence": {"display": {"title": {"text": "Feeling the Power", "color": "aqua"}, "description": {"text": "Obtain a Stardust Essence", "color": "green"}}, "parent": f"{ns}:visible/stuff/stardust_ingot"},
-		"stuff/stardust_core": {"display": {"title": {"text": "Stardust Technology", "color": "aqua"}, "description": {"text": "Obtain a Stardust Core", "color": "green"}}, "parent": f"{ns}:visible/stuff/stardust_essence"},
-		"stuff/awakened_stardust_block": {"display": {"title": {"text": "A respectful Block Type", "color": "aqua"}, "description": {"text": "Obtain an Awakened Stardust Block", "color": "green"}}, "parent": f"{ns}:visible/stuff/awakened_stardust"},
-		"stuff/awakened_stardust": {"display": {"title": {"text": "Stardust Evolution", "color": "aqua"}, "description": {"text": "Obtain an Awakened Stardust", "color": "green"}}, "parent": f"{ns}:visible/stuff/stardust_essence"},
-		"stuff/ultimate_shard": {"display": {"title": {"text": "A Burst of Energy", "color": "aqua"}, "description": {"text": "Obtain an Ultimate Shard", "color": "green"}}, "parent": f"{ns}:visible/stuff/compacted_stardust_shard"},
+		"stuff/dragon_pearl": {"display": {"title": {"text": "Faster and Farther", "color": "aqua"}}, "parent": f"{ns}:visible/stuff/stardust_essence"},
+		"stuff/ender_dragon_pearl": {"display": {"title": {"text": "Even Faster and Farther", "color": "aqua"}}, "parent": f"{ns}:visible/stuff/dragon_pearl"},
+		"stuff/stardust_block": {"display": {"title": {"text": "Is that Stardust Diamond?", "color": "aqua"}}, "parent": f"{ns}:visible/stuff/stardust_ingot"},
+		"stuff/compacted_stardust_shard": {"display": {"title": {"text": "A costly Stardust Material", "color": "aqua"}}, "parent": f"{ns}:visible/stuff/stardust_block"},
+		"stuff/stardust_ingot": {"display": {"title": {"text": "I feel like it's useful", "color": "aqua"}}, "parent": f"{ns}:visible/root"},
+		"stuff/stardust_essence": {"display": {"title": {"text": "Feeling the Power", "color": "aqua"}}, "parent": f"{ns}:visible/stuff/stardust_ingot"},
+		"stuff/stardust_core": {"display": {"title": {"text": "Stardust Technology", "color": "aqua"}}, "parent": f"{ns}:visible/stuff/stardust_essence"},
+		"stuff/awakened_stardust_block": {"display": {"title": {"text": "A respectful Block Type", "color": "aqua"}}, "parent": f"{ns}:visible/stuff/awakened_stardust"},
+		"stuff/awakened_stardust": {"display": {"title": {"text": "Stardust Evolution", "color": "aqua"}}, "parent": f"{ns}:visible/stuff/stardust_essence"},
+		"stuff/ultimate_shard": {"display": {"title": {"text": "A Burst of Energy", "color": "aqua"}}, "parent": f"{ns}:visible/stuff/compacted_stardust_shard"},
 		**{
-			f"stuff/{material}_{form}": {"display": {"title": {"text": f"{title} {ROMAN_NUMERALS[i]}", "color": "aqua"}, "description": {"text": f"Obtain a {name}", "color": "green"}}, "parent": parent}
+			f"stuff/{material}_{form}": {"display": {"title": {"text": f"{title} {ROMAN_NUMERALS[i]}", "color": "aqua"}}, "parent": parent}
 			for material, title in [
 				("legendarium", "Feeling the Breeze"),
 				("solarium", "Feeling the Sun"),
 				("darkium", "Feeling the Shadow")
 			]
 			for i, form in enumerate(("fragment", "ingot", "block"))
-			for name in (f"{material.title()} {form.title()}",)
 			for parent in (f"{ns}:visible/stuff/original_stardust_equipment" if i == 0 else f"{ns}:visible/stuff/{material}_{('fragment' if i == 1 else 'ingot')}",)
-		}
+		},
 
-		# Technology advancements
-		# TODO
+
+
+		## Technology advancements
+		"technology/simplenergy_manual": {"display": {"icon": {"id": "minecraft:book", "components": {"minecraft:item_model":"simplenergy:manual"}}, "title": {"text": "Tecnología", "color": "aqua"}, "description": {"text": "Following advancements are related to technology", "color": "green"},"frame":"goal","show_toast":False,"announce_to_chat":False}, "parent": f"{ns}:visible/root", "criteria": criteria_item("stardust_fragment")},
+		"technology/stardust_cable": {"display": {"title": {"text": "Energy Transfer I", "color": "aqua"}}, "parent": f"{ns}:visible/technology/simplenergy_manual"},
+		"technology/awakened_stardust_cable": {"display": {"title": {"text": "Energy Transfer II", "color": "aqua"}}, "parent": f"{ns}:visible/technology/stardust_cable"},
+		"technology/ultimate_cable": {"display": {"title": {"text": "Energy Transfer III", "color": "aqua"}}, "parent": f"{ns}:visible/technology/awakened_stardust_cable"},
+		"technology/stardust_battery": {"display": {"title": {"text": "Stardust Energy I", "color": "aqua"}}, "parent": f"{ns}:visible/technology/simplenergy_manual"},
+		"technology/awakened_stardust_battery": {"display": {"title": {"text": "Stardust Energy II", "color": "aqua"}}, "parent": f"{ns}:visible/technology/stardust_battery"},
+		"technology/ultimate_battery": {"display": {"title": {"text": "Stardust Energy III", "color": "aqua"}}, "parent": f"{ns}:visible/technology/awakened_stardust_battery"},
+		"technology/stardust_solar_panel": {"display": {"title": {"text": "Greater Photosensitivity I", "color": "aqua"}}, "parent": f"{ns}:visible/technology/simplenergy_manual"},
+		"technology/awakened_stardust_solar_panel": {"display": {"title": {"text": "Greater Photosensitivity II", "color": "aqua"}}, "parent": f"{ns}:visible/technology/stardust_solar_panel"},
+		"technology/legendarium_solar_panel": {"display": {"title": {"text": "Breeze Photosensitivity", "color": "aqua"}}, "parent": f"{ns}:visible/technology/awakened_stardust_solar_panel"},
+		"technology/solarium_solar_panel": {"display": {"title": {"text": "Burning Photosensitivity", "color": "aqua"}}, "parent": f"{ns}:visible/technology/legendarium_solar_panel"},
+		"technology/darkium_solar_panel": {"display": {"title": {"text": "Anti-Photosensitivity", "color": "aqua"}}, "parent": f"{ns}:visible/technology/solarium_solar_panel"},
+		"technology/ultimate_solar_panel": {"display": {"title": {"text": "Ultimate Photosensitivity", "color": "aqua"}}, "parent": f"{ns}:visible/technology/darkium_solar_panel"},
+		"technology/quarry_configurator": {"display": {"title": {"text": "Control of the Mining Industry", "color": "aqua"}}, "parent": f"{ns}:visible/technology/simplenergy_manual"},
+		**{
+			f"technology/quarry/{item}": {"display": {"title": {"text": f"Mining Industry {roman}", "color": "aqua"}, "frame": frame}, "parent": parent}
+			for i in range(len(QUARRY_STATS))
+			for frame in ("task" if i < (len(QUARRY_STATS) - 1) else "challenge",)
+			for item, _, roman in (quarry_display(i),)
+			for parent in (f"{ns}:visible/technology/quarry_configurator" if i == 0 else f"{ns}:visible/technology/quarry/{quarry_display(i-1)[0]}",)
+		},
+		"technology/growth_accelerator": {"display": {"title": {"text": "Speeding up Nature", "color": "aqua"}}, "parent": f"{ns}:visible/technology/simplenergy_manual"},
+		"technology/mob_grinder": {"display": {"title": {"text": "Automated Combat", "color": "aqua"}}, "parent": f"{ns}:visible/technology/growth_accelerator"},
+		"technology/nether_star_generator": {"display": {"title": {"text": "Nether Star Power", "color": "aqua"}}, "parent": f"{ns}:visible/technology/mob_grinder"},
+		"technology/advanced_furnace_generator": {"display": {"title": {"text": "Carbon Dioxide I", "color": "aqua"}}, "parent": f"{ns}:visible/technology/simplenergy_manual"},
+		"technology/stardust_furnace_generator": {"display": {"title": {"text": "Carbon Dioxide II", "color": "aqua"}}, "parent": f"{ns}:visible/technology/advanced_furnace_generator"},
+		"technology/awakened_stardust_furnace_generator": {"display": {"title": {"text": "Carbon Dioxide III", "color": "aqua"}}, "parent": f"{ns}:visible/technology/stardust_furnace_generator"},
 	}
 
 	# Add default display options
@@ -230,14 +273,20 @@ def add_visible_advancements() -> None:
 		if not adv.get("display", {}).get("icon"):
 			advancement["display"]["icon"] = def_to_icon(item)
 
+		# Set description, if not already set
+		if not adv.get("display", {}).get("description"):
+			advancement["display"]["description"] = default_description(item)
+
 		# Set the criteria, if not already set
 		if not adv.get("criteria"):
-			criteria: dict[str, Any] = {"requirement": {"trigger": "minecraft:inventory_changed","conditions": {"items": [{"predicates": {}}]}}}
-			criteria["requirement"]["conditions"]["items"][0]["predicates"]["minecraft:custom_data"] = {ns:{item:True}}
-			advancement["criteria"] = criteria
+			advancement["criteria"] = criteria_item(item)
+
+		# Merge the advancement with specific order
+		advancement = super_merge_dict(advancement, adv)
+		advancement["display"] = sort_dict(advancement["display"], ["icon", "title", "description", "background", "frame", "show_toast", "announce_to_chat", "hidden"])
+		advancement = sort_dict(advancement, ["display", "criteria", "requirements", "rewards", "parent"])
 
 		# Set the advancement
-		advancement = super_merge_dict(advancement, adv)
 		Mem.ctx.data[ns].advancements[f"visible/{full_path}"] = set_json_encoder(Advancement(advancement), max_level = 7)
 
 	return
